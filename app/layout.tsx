b1478a7f80fedac09b3b3fd0814ca22c1eca45d4
Stan Chen <stanchensz@gmail.com>
2024-06-08 10:29:15 -0600

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
+import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <div style={{ margin: '0 auto', maxWidth: '960px', padding: '20px'}}>
-          <header style={{ display: 'flex', alignItems: 'center', userSelect: 'none' }}>
-            <img
-              src="/favicon.ico"
-              alt="Favicon"
-              style={{ height: '48px', width: '48px', marginRight: '10px' , userSelect: 'none'}} // Adjust height and width statically
-            />
-            <h1 style={{ fontSize: '24px'}}>GPT Mini</h1> {/* Assuming a default font size */}
-          </header>
+        <div style={{ margin: '0 auto', maxWidth: '960px', padding: '20px' }}>
+          <div className="title-container">
+            <img src="/logo.png" alt="Logo" className="logo" width="60" height="60" />
+            <h1 className="title">GPTMINI</h1>
+          </div>
           <main>{children}</main>
         </div>
       </body>
